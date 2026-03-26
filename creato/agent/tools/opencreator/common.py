8d1f076908ba76e3c89765fcad4c0a25c805fe61
niuzj <niuzj0@gmail.com>
2026-03-26 23:06:06 +0800

    fix: 更新 API 基础 URL 为开发环境地址

--- creato/agent/tools/opencreator/common.py
+++ creato/agent/tools/opencreator/common.py
@@ -11,7 +11,7 @@ from uuid import uuid4
 
 
 
-_API_BASE = "https://api-develop.opencreator.io"
+_API_BASE = "https://dev-railway.opencreator.io"
 _EDITOR_BASE = "https://editor-dev.opencreator.io"
 _NODE_WIDTH = 360.0
 _DEFAULT_NODE_HEIGHT = 280.0
